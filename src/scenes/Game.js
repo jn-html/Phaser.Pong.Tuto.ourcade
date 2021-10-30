c1dd234d129d3a11d5cfccae991680a2809668e2
jn-html <marmainjean@gmail.com>
2021-10-30 16:29:03 +0200

    Set up & start pong project

---
+++ src/scenes/Game.js
@@ -0,0 +1,29 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+
+  preload() {
+
+  }
+
+  create() {
+
+
+    const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+    this.physics.add.existing(ball)
+    ball.body.setBounce(1,1)
+
+    ball.body.setCollideWorldBounds(true, 1, 1)
+
+    ball.body.setVelocity(-200, 0)
+
+    const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
+    this.physics.add.existing(paddleLeft, true)
+    
+    /**@type {Phaser.Physics.Arcade.Body} */
+
+    this.physics.add.collider(paddleLeft ,ball)
+    
+  }
+
+}
