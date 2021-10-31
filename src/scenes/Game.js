bdba5328930b8620e361f66f377836c9f5520e2c
jn-html <marmainjean@gmail.com>
2021-10-31 15:11:47 +0100

    Add paddle Right(AI) and AI mouvement logic

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,10 @@ import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
 
+  init() {
+    this.paddleRightVelocity = new Phaser.Math.Vector2(0, 0)
+  }
+
   preload() {
 
   }
@@ -9,26 +13,31 @@ export default class Game extends Phaser.Scene {
   create() {
 
 
-    const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-    this.physics.add.existing(ball)
-    ball.body.setBounce(1,1)
+    this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+    this.physics.add.existing(this.ball)
+    this.ball.body.setBounce(1,1)
 
-    ball.body.setCollideWorldBounds(true, 1, 1)
+    this.ball.body.setCollideWorldBounds(true, 1, 1)
 
-    ball.body.setVelocity(-200, 0)
+    this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200), 1)
 
     this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
     this.physics.add.existing(this.paddleLeft, true)
     
+    this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
+    this.physics.add.existing(this.paddleRight, true)
+    
     /**@type {Phaser.Physics.Arcade.Body} */
 
-    this.physics.add.collider(this.paddleLeft ,ball)
+    this.physics.add.collider(this.paddleLeft ,this.ball)
+    this.physics.add.collider(this.paddleRight ,this.ball)
     
     this.cursors = this.input.keyboard.createCursorKeys()
     
   }
 
   update() {
+    
     /**@type {Phaser.Physics.Arcade.Body} */
     
     const body = this.paddleLeft.body
@@ -41,6 +50,30 @@ export default class Game extends Phaser.Scene {
       this.paddleLeft.y += 10
       body.updateFromGameObject()
     }
+
+    const diff = this.ball.y - this.paddleRight.y
+    if (Math.abs(diff) < 10) {
+      return
+    }
+
+    const aiSpeed = 3
+    if (diff < 0) {
+      // ball above paddle
+      this.paddleRightVelocity.y = -aiSpeed
+      if (this.paddleRightVelocity.y < -10) {
+        this.paddleRightVelocity.y = -10
+      }
+    } 
+    else if (diff > 0) {
+      // ball below paddle
+      this.paddleRightVelocity.y = aiSpeed
+      if (this.paddleRightVelocity.y > 10) {
+        this.paddleRightVelocity.y = 10
+      }
+    }
+
+    this.paddleRight.y += this.paddleRightVelocity.y
+    this.paddleRight.body.updateFromGameObject()
     
   }
 
